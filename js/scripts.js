58e404b4d185dac39d1b31455f8fbd1a3f679efc
Matt <m_dittmann@yahoo.com>
2017-11-07 22:35:33 -0800

    update js

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,20 @@
 function checkUsername(e) {
   // get target of event
   var target = e.target;
+  console.log(target);
 }
 
 var el = document.getElementById('username');
 el.addEventListener('blur', checkUsername, false);
+
+
+// EVENT LISTENER WITH PARAMETERS
+function checkUsername(e, minLength) {
+  // get target of event
+  var target = e.target;
+}
+
+var el = document.getElementById('username');
+el.addEventListener('blur', function(e) {
+  checkUsername(e, 5);
+}, false);
